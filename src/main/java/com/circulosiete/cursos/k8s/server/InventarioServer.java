72af642bd52cd00d17d721db612d68d201ace9ed
kanolato <kanolato@gmail.com>
2017-09-28 14:53:29 -0500

    inicio rabbit

--- src/main/java/com/circulosiete/cursos/k8s/server/InventarioServer.java
+++ src/main/java/com/circulosiete/cursos/k8s/server/InventarioServer.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.grpc.spring.boot.autoconfigure.annotation.EnableGRpcServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PreDestroy;
+
 @SpringBootApplication
 @EnableGRpcServer
 public class InventarioServer {
   public static void main(String[] args) {
     SpringApplication.run(InventarioServer.class, args);
+  }
+
+  @PreDestroy
+  public void Destroy(){
+    System.out.println("mamo...");
   }
 }
--- src/main/java/com/circulosiete/cursos/k8s/server/InventarioService.java
+++ src/main/java/com/circulosiete/cursos/k8s/server/InventarioService.java
@@ -8,18 +8,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.grpc.spring.boot.autoconfigure.annotation.GRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.circulosiete.cursos.k8s.server.service.Sender;
+
+import java.io.IOException;
 
 @Slf4j
 @GRpcService
 public class InventarioService extends InventarioServiceGrpc.InventarioServiceImplBase{
 
   private Logger logger = LoggerFactory.getLogger(InventarioService.class);
+  private final Sender sender;
+
+  public InventarioService(Sender sender){
+    this.sender = sender;
+  }
 
   @Override
   public void inventarioHandler(InventarioRequest request, StreamObserver<InventarioResponse> responseObserver){
 
     // analizar request
     System.out.println(request);
+    sender.sendToRabbitmq("mensaje lol xd");
     logger.debug("Request " + request);
 
     // se manda respuesta
