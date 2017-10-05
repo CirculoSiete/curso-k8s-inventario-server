b064f2c02d38e74df4b4898c7f50fa2316fb6fc5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-10-05 17:14:10 -0500

    corrigiendo constructor perdido

--- src/main/java/com/circulosiete/cursos/k8s/server/model/Product.java
+++ src/main/java/com/circulosiete/cursos/k8s/server/model/Product.java
@@ -3,6 +3,8 @@ package com.circulosiete.cursos.k8s.server.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Tolerate;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -12,7 +14,13 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Builder
+@Slf4j
 public class Product {
+
+  @Tolerate
+  public Product() {
+    log.debug("Creating a product!");
+  }
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
