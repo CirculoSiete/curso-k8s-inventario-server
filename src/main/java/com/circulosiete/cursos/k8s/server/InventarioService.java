7d9c1c26ee5969f2bdf34f84898918ffeac56f6b
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-10-05 11:45:21 -0500

    wip

--- src/main/java/com/circulosiete/cursos/k8s/server/InventarioService.java
+++ src/main/java/com/circulosiete/cursos/k8s/server/InventarioService.java
@@ -1,25 +1,26 @@
 package com.circulosiete.cursos.k8s.server;
 
 import com.circulosiete.cursos.k8s.*;
-import com.circulosiete.cursos.k8s.server.service.Sender;
 import com.circulosiete.cursos.k8s.server.model.Producto;
 import com.circulosiete.cursos.k8s.server.repo.ProductoRepository;
-
+import com.circulosiete.cursos.k8s.server.service.Sender;
 import com.circulosiete.cursos.k8s.server.service.Validacion;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.grpc.spring.boot.autoconfigure.annotation.GRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+
+import java.math.BigDecimal;
 
 
 @Slf4j
 @GRpcService
-public class InventarioService extends InventarioServiceGrpc.InventarioServiceImplBase{
+public class InventarioService extends InventarioServiceGrpc.InventarioServiceImplBase {
 
   private Logger logger = LoggerFactory.getLogger(InventarioService.class);
   private final Sender sender;
@@ -27,19 +28,26 @@ public class InventarioService extends InventarioServiceGrpc.InventarioServiceIm
   private Validacion validacion;
   private Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
-  public InventarioService(Sender sender, ProductoRepository productoRepository, Validacion validacion){
+  public InventarioService(Sender sender, ProductoRepository productoRepository, Validacion validacion) {
     this.sender = sender;
     this.productoRepository = productoRepository;
     this.validacion = validacion;
   }
 
   @Override
-  public void inventarioCreate(InventarioRequest request, StreamObserver<InventarioResponse> responseObserver){
+  public void inventarioCreate(InventarioRequest request, StreamObserver<InventarioResponse> responseObserver) {
     boolean aceptado = false;
     // se guarda en postgres
-    if (validacion.createValidacion(request.getNombre())){
-      System.out.println("es valido");
-      productoRepository.save(new Producto(request.getNombre(), request.getDescripcion(), request.getPrecio()));
+    if (validacion.createValidacion(request.getNombre())) {
+      log.info("es valido");
+
+      Producto product = Producto.builder()
+        .name(request.getNombre())
+        .description(request.getDescripcion())
+        .price(BigDecimal.valueOf(request.getPrecio()))
+        .build();
+
+      productoRepository.save(product);
 
       // se crea json para encolar
       JsonObject queueObject = new JsonObject();
@@ -56,22 +64,22 @@ public class InventarioService extends InventarioServiceGrpc.InventarioServiceIm
       sender.sendToRabbitmq(gson.toJson(queueJson));
 
       aceptado = true;
-    }else{
+    } else {
       System.out.println("no es valido");
       aceptado = false;
     }
 
     // se manda respuesta cliente GRPC
     responseObserver.onNext(InventarioResponse.newBuilder()
-            .setAceptado(aceptado)
-            .build());
+      .setAceptado(aceptado)
+      .build());
 
     // se cierra canal GRPC
     responseObserver.onCompleted();
   }
 
   @Override
-  public void inventarioDelete(IdRequest request, StreamObserver<InventarioResponse> responseObserver){
+  public void inventarioDelete(IdRequest request, StreamObserver<InventarioResponse> responseObserver) {
     // se borra en postgres
     Producto producto = productoRepository.findOne(request.getId());
     productoRepository.delete(request.getId());
@@ -80,9 +88,9 @@ public class InventarioService extends InventarioServiceGrpc.InventarioServiceIm
     // se crea json para encolar
     JsonObject queueObject = new JsonObject();
     queueObject.addProperty("id", request.getId());
-    queueObject.addProperty("nombre", producto.getNombre());
-    queueObject.addProperty("descripcion", producto.getDescripcion());
-    queueObject.addProperty("precio", producto.getPrecio());
+    queueObject.addProperty("nombre", producto.getName());
+    queueObject.addProperty("descripcion", producto.getDescription());
+    queueObject.addProperty("precio", producto.getPrice());
 
     JsonObject queueJson = new JsonObject();
     queueJson.addProperty("tipo", "producto");
@@ -95,44 +103,44 @@ public class InventarioService extends InventarioServiceGrpc.InventarioServiceIm
 
     // se manda respuesta cliente GRPC
     responseObserver.onNext(InventarioResponse.newBuilder()
-            .setAceptado(true)
-            .build());
+      .setAceptado(true)
+      .build());
 
     // se cierra canal GRPC
     responseObserver.onCompleted();
   }
 
   @Override
-  public void inventarioGet(IdRequest request, StreamObserver<GetResponse> responseObserver){
+  public void inventarioGet(IdRequest request, StreamObserver<GetResponse> responseObserver) {
     // se obtiene en postgres
     Producto producto = productoRepository.findOne(request.getId());
 
     // se manda respuesta cliente GRPC
     responseObserver.onNext(GetResponse.newBuilder()
-            .setId(producto.getId())
-            .setNombre(producto.getNombre())
-            .setDescripcion(producto.getDescripcion())
-            .setPrecio(producto.getPrecio())
-            .build());
+      .setId(producto.getId())
+      .setNombre(producto.getName())
+      .setDescripcion(producto.getDescription())
+      .setPrecio(producto.getPrice().intValue())
+      .build());
     // se cierra canal GRPC
     responseObserver.onCompleted();
   }
 
   @Override
-  public void inventarioUpdate(GetResponse request, StreamObserver<InventarioResponse> responseObserver){
+  public void inventarioUpdate(GetResponse request, StreamObserver<InventarioResponse> responseObserver) {
     // se busca y actualiza
     Producto producto = productoRepository.findOne(request.getId());
-    producto.setNombre(request.getNombre());
-    producto.setDescripcion(request.getDescripcion());
-    producto.setPrecio(request.getPrecio());
+    producto.setName(request.getNombre());
+    producto.setDescription(request.getDescripcion());
+    producto.setPrice(BigDecimal.valueOf(request.getPrecio()));
     productoRepository.save(producto);
 
     // se crea json para encolar
     JsonObject queueObject = new JsonObject();
     queueObject.addProperty("id", producto.getId());
-    queueObject.addProperty("nombre", producto.getNombre());
-    queueObject.addProperty("descripcion", producto.getDescripcion());
-    queueObject.addProperty("precio", producto.getPrecio());
+    queueObject.addProperty("nombre", producto.getName());
+    queueObject.addProperty("descripcion", producto.getDescription());
+    queueObject.addProperty("precio", producto.getPrice());
 
     JsonObject queueJson = new JsonObject();
     queueJson.addProperty("tipo", "producto");
@@ -145,8 +153,8 @@ public class InventarioService extends InventarioServiceGrpc.InventarioServiceIm
 
     // se manda respuesta cliente GRPC
     responseObserver.onNext(InventarioResponse.newBuilder()
-            .setAceptado(true)
-            .build());
+      .setAceptado(true)
+      .build());
 
     // se cierra canal GRPC
     responseObserver.onCompleted();
--- src/main/java/com/circulosiete/cursos/k8s/server/model/Producto.java
+++ src/main/java/com/circulosiete/cursos/k8s/server/model/Producto.java
@@ -1,64 +1,27 @@
 package com.circulosiete.cursos.k8s.server.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
-@Table(name = "producto")
+@Table(name = "products")
+@Getter
+@Setter
+@Builder
 public class Producto {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
 
-  @Column(name = "nombre")
-  private String nombre;
+  private String name;
 
-  @Column(name = "descripcion")
-  private String descripcion;
+  private String description;
 
-  @Column(name = "precio")
-  private int precio;
-
-  protected Producto() {}
-
-  public Producto(String nombre, String descripcion, int precio) {
-    this.nombre = nombre;
-    this.descripcion = descripcion;
-    this.precio = precio;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public String getNombre() {
-    return nombre;
-  }
-
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-
-
-  public String getDescripcion() {
-    return descripcion;
-  }
-
-  public void setDescripcion(String descripcion) {
-    this.descripcion = descripcion;
-  }
-
-  public int getPrecio() {
-    return precio;
-  }
-
-  public void setPrecio(int precio) {
-    this.precio = precio;
-  }
+  private BigDecimal price;
 
 }
