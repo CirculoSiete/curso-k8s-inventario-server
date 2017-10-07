e60fe5132c70ffa9e1805015bdc2529521c6a9b3
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-10-07 01:30:24 -0500

    mejoras en el contrato e implementacion

--- src/main/java/com/circulosiete/cursos/k8s/warehouse/grpc/GrpcInventarioService.java
+++ src/main/java/com/circulosiete/cursos/k8s/warehouse/grpc/GrpcInventarioService.java
@@ -1,10 +1,6 @@
 package com.circulosiete.cursos.k8s.warehouse.grpc;
 
-import com.circulosiete.cursos.k8s.EntityId;
-import com.circulosiete.cursos.k8s.InventarioServiceGrpc;
-import com.circulosiete.cursos.k8s.ProductRequest;
-import com.circulosiete.cursos.k8s.ProductResponse;
-import com.circulosiete.cursos.k8s.warehouse.model.Product;
+import com.circulosiete.cursos.k8s.*;
 import com.circulosiete.cursos.k8s.warehouse.repo.ProductRepository;
 import com.circulosiete.cursos.k8s.warehouse.service.ProductCatalogService;
 import io.grpc.stub.StreamObserver;
@@ -26,43 +22,27 @@ public class GrpcInventarioService extends InventarioServiceGrpc.InventarioServi
     this.productRepository = productRepository;
   }
 
-
   @Override
   public void create(ProductRequest request, StreamObserver<ProductResponse> responseObserver) {
-
-    productCatalogService.add(from(request))
-      .ifPresent(newProduct -> responseObserver.onNext(from(newProduct)));
-
+    responseObserver.onNext(from(productCatalogService.add(from(request))));
     responseObserver.onCompleted();
   }
 
   @Override
   public void read(EntityId request, StreamObserver<ProductResponse> responseObserver) {
-
-    Product product = productRepository.findById(request.getId())
-      .orElse(null);
-
-    responseObserver.onNext(from(product));
-
+    responseObserver.onNext(from(productRepository.findById(request.getId())));
     responseObserver.onCompleted();
   }
 
   @Override
-  public void update(ProductResponse request, StreamObserver<ProductResponse> responseObserver) {
-    Product product = from(request);
-
-    productCatalogService.update(request.getId(), product)
-      .ifPresent(product1 -> responseObserver.onNext(from(product1)));
-
+  public void update(ProductModel request, StreamObserver<ProductResponse> responseObserver) {
+    responseObserver.onNext(from(productCatalogService.update(request.getId(), from(request))));
     responseObserver.onCompleted();
   }
 
   @Override
   public void delete(EntityId request, StreamObserver<ProductResponse> responseObserver) {
-
-    productCatalogService.delete(request.getId())
-      .ifPresent(product -> responseObserver.onNext(from(product)));
-
+    responseObserver.onNext(from(productCatalogService.delete(request.getId())));
     responseObserver.onCompleted();
   }
 
--- src/main/java/com/circulosiete/cursos/k8s/warehouse/grpc/ModelUtil.java
+++ src/main/java/com/circulosiete/cursos/k8s/warehouse/grpc/ModelUtil.java
@@ -1,10 +1,12 @@
 package com.circulosiete.cursos.k8s.warehouse.grpc;
 
+import com.circulosiete.cursos.k8s.ProductModel;
 import com.circulosiete.cursos.k8s.ProductRequest;
 import com.circulosiete.cursos.k8s.ProductResponse;
 import com.circulosiete.cursos.k8s.warehouse.model.Product;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 public class ModelUtil {
   public static Product from(ProductRequest request) {
@@ -15,7 +17,7 @@ public class ModelUtil {
       .build();
   }
 
-  public static Product from(ProductResponse request) {
+  public static Product from(ProductModel request) {
     return Product.builder()
       .description(request.getDescripcion())
       .name(request.getNombre())
@@ -23,12 +25,25 @@ public class ModelUtil {
       .build();
   }
 
-  public static ProductResponse from(Product product) {
-    return ProductResponse.newBuilder()
-      .setNombre(product.getName())
-      .setId(product.getId())
-      .setDescripcion(product.getDescription())
-      .setPrecio(product.getPrice().intValue())
-      .build();
+  public static Product from(ProductResponse request) {
+    return from(request.getProduct());
+  }
+
+  public static ProductResponse from(Optional<Product> found) {
+
+    ProductResponse.Builder builderResponse = ProductResponse.newBuilder();
+
+    found.ifPresent(product -> {
+      ProductModel productModel = ProductResponse.newBuilder().getProductBuilder()
+        .setNombre(product.getName())
+        .setId(product.getId())
+        .setDescripcion(product.getDescription())
+        .setPrecio(product.getPrice().intValue())
+        .build();
+
+      builderResponse.setProduct(productModel);
+    });
+
+    return builderResponse.build();
   }
 }
