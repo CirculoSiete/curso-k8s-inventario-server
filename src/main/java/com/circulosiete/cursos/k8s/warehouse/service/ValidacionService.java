6ab28e4d0ac7d9da71357b8f73707cf5ff6564e5
kanolato <kanolato@gmail.com>
2017-10-09 22:25:28 -0500

    removido codigo duro validationsvc

--- src/main/java/com/circulosiete/cursos/k8s/warehouse/service/ValidacionService.java
+++ src/main/java/com/circulosiete/cursos/k8s/warehouse/service/ValidacionService.java
@@ -1,22 +1,32 @@
 package com.circulosiete.cursos.k8s.warehouse.service;
 
-import com.circulosiete.cursos.k8s.CreateRequest;
 import com.circulosiete.cursos.k8s.ValidacionServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ValidacionService {
 
-  //TODO: remover el codigo duro. Estos valores deben ir en configuracion
-  private ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 7070)
-    .usePlaintext(true)
-    .build();
+  private  String hostname;
+  private int port;
+  private ManagedChannel channel;
+  private ValidacionServiceGrpc.ValidacionServiceBlockingStub stub;
+  @Autowired
+  public ValidacionService(@Value("${validation-svc.hostname}") String hostname,
+                           @Value("${validation-svc.port}") int port){
+    this.hostname = hostname;
+    this.port = port;
+    channel = ManagedChannelBuilder.forAddress(hostname, port)
+            .usePlaintext(true)
+            .build();
+    stub = ValidacionServiceGrpc.newBlockingStub(channel);
 
-  private ValidacionServiceGrpc.ValidacionServiceBlockingStub stub = ValidacionServiceGrpc.newBlockingStub(channel);
+  }
 
   public boolean createValidacion(String nombre) {
 
