93521f074a0e307332a4f69114034b92acb22a9a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-10-13 16:20:37 -0500

    agregado modulo contract

---
+++ contract/src/test/java/com/circulosiete/cursos/k8s/warehouse/repo/ProductRepositoryTests.java
@@ -0,0 +1,16 @@
+package com.circulosiete.cursos.k8s.warehouse.repo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import java.math.BigDecimal;
+
+
+public class ProductRepositoryTests {
+
+  @Test
+  public void whenFindByName_thenReturnProduct() {
+  }
+
+}
