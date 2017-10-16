84fcc3ff821da6143f1c440f18cf0b98fdb95e94
Domingo Suarez Torres <domingo.suarez@gmail.com>
2017-10-16 18:09:59 -0500

    removido el codigo duro del endpoint de Productos

--- rest_gateway/main/main.go
+++ rest_gateway/main/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 
+	"github.com/caarlos0/env"
 	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"golang.org/x/net/context"
@@ -12,12 +14,26 @@ import (
 	gw "github.com/CirculoSiete/productGateway/warehouse"
 )
 
-var (
-	//TODO: sacar esto a variables de ambiente
-	productEndpoint = flag.String("product_endpoint", "localhost:6565", "endpoint of Products")
-)
+type config struct {
+	Port            int    `env:"PORT" envDefault:"9090"`
+	ProductEndpoint string `env:"PRODUCT_ENDPOINT" envDefault:"localhost"`
+	ProductPort     int    `env:"PRODUCT_PORT" envDefault:"6565"`
+}
 
 func run() error {
+	cfg := config{}
+	errParse := env.Parse(&cfg)
+	if errParse != nil {
+		fmt.Printf("%+v\n", errParse)
+	}
+	fmt.Printf("%+v\n", cfg)
+
+	var (
+		endpoint = fmt.Sprintf("%s:%d", cfg.ProductEndpoint, cfg.ProductPort)
+		productEndpoint = flag.String("product_endpoint", endpoint, "endpoint of Products")
+	)
+	fmt.Printf("%s:%s\n", "Product Endpoint: ", endpoint)
+
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -29,8 +45,9 @@ func run() error {
 		return err
 	}
 
-	//TODO: sacar esto a variables de ambiente
-	return http.ListenAndServe(":9090", mux)
+	listen := fmt.Sprintf(":%d", cfg.Port)
+	fmt.Printf("%s:%s\n", "Listening... ", listen)
+	return http.ListenAndServe(listen, mux)
 }
 
 func main() {
